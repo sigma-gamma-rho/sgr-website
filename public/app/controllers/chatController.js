500e9eb66a7e45f5d22ec232e99f495ee1fdfa6d
aumulloa <aumulloa@gmail.com>
2016-02-22 14:15:19 -0500

    added files for chat controller

---
+++ public/app/controllers/chatController.js
@@ -0,0 +1,16 @@
+angular.module('chatController', [])
+
+// ==============================================
+// INJECT DEPENDENCIES
+// ==============================================
+.controller('chatController', function($location) {
+
+  var chat = this;
+
+
+	chat.initChat = function() {
+
+
+	};
+
+});
